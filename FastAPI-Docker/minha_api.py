95708154f6ae56cd1b52b15849420f53dbe8fa6d
LucaSarhan <99192966+LucaSarhan@users.noreply.github.com>
2023-09-20 13:53:28 -0300

    avanços

--- FastAPI-Docker/minha_api.py
+++ FastAPI-Docker/minha_api.py
@@ -1,12 +1,56 @@
 # -*- coding: utf-8 -*-
-
+import streamlit as st
+import numpy as np
 import pandas as pd
 from pycaret.regression import load_model, predict_model
-from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel, create_model
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from typing import ClassVar
+import os
+import psycopg2
+import requests
+from fastapi import FastAPI, Request, Form, HTTPException
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import jwt
+import streamlit as st
+
+users = {
+    "teste": "teste123",
+}
+
+def verify_user(username, password):
+    if username not in users:
+        return False
+    if users[username] != password:
+        return False
+    return True
+
+secret_key = "SecretCode"
+
+def generate_token(user):
+    payload = {"username": user}
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
+    return token
+
+def verify_token(token):
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=["HS256"])
+        username = payload["username"]
+        return username
+    except:
+        return None
+
+
+db_conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="postgres",
+    host="localhost",
+    port="5432"
+)
 
 class InputModel(BaseModel):
     data : datetime
@@ -71,6 +115,10 @@ app = FastAPI()
 # Load trained Pipeline
 model = load_model("minha_api")
 
+@app.get("/")
+async def get_html(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "index"})
+
 # Define predict function
 @app.post("/predict", response_model=OutputModel)
 def predict(data: InputModel):
@@ -84,3 +132,7 @@ def predict(data: InputModel):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+dirname = os.path.dirname(__file__)
+app.mount("/assets", StaticFiles(directory=os.path.join(dirname, 'assets')), name="assets")
+templates = Jinja2Templates(directory=os.path.join(dirname, 'templates'))
